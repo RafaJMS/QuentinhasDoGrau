79be022a1fd34cd9259e46c773e60951b1d72dda
RafaJMS <rafaeljmecenas1@gmail.com>
2024-02-01 22:35:10 -0300

    Responsividade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ export default function Home(){
             <div className="home-box">
             <h1 className="title">Bem vindo ao Quentinhas do Grau !</h1>
             
-            <article className="text-home"> 
+            <article className="text"> 
                 Seja muito bem vindo ao Quentinhas do Grau, o lugar perfeito para
                 sua refeição diária. Os melhores preços e sabores, você encontra aqui!
             </article>
@@ -25,7 +25,14 @@ export default function Home(){
                 <Link id="section-link" to={"/bebidas"}><span className="section-item">Bebidas</span></Link>
 
             </div>
-
+            <span className="cardapio"> Onde nos encontrar: </span>
+            <div className="localizacao">
+                <article className="textf">
+                Deseja nos fazer uma visita? Você pode nos encontrar no endereço: 
+                </article>
+                <span className="endereco">Rua das Flores, 123, Bairro das Rosas, Cidade das Cores.</span>
+            </div>
+            <span className="contato">Contato: (&#) 4002 - 8922</span>
             </div>
             
                 
